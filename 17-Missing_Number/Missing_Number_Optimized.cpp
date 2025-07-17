3e68807abee890dbf45f81adcb44f59b8fb98b8d
Priyanshu Gupta <pg0000742@gmail.com>
2025-07-17 16:46:48 +0530

    Missing Number Question is Done Co-authored-by: Abhay raj <abhay02delhi@gmail.com>

---
+++ 17-Missing_Number/Missing_Number_Optimized.cpp
@@ -0,0 +1,14 @@
+// time : O(n)
+// space : O(1)
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = nums.size();
+        
+        for (int i = 0; i < nums.size(); i++) {
+            res += i - nums[i];
+        }
+        
+        return res;        
+    }
+};
