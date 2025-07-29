ccda2c963987518b3664a19050345297b683b8ac
Priyanshu Gupta <pg0000742@gmail.com>
2025-07-29 23:59:03 +0530

    Majority Element Question is Done Co-authored-by: Abhay Raj <abhay02delhi@gmail.com>

---
+++ 32-Majority_Element/Majority_Element_O.cpp
@@ -0,0 +1,18 @@
+// time : O(n)
+// space : O(1)
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+};
