f3e033d8d893635e59939dc87d3bb4cef2a51a90
Priyanshu Gupta <pg0000742@gmail.com>
2025-08-03 21:54:40 +0530

    Search Insert Position Co-authored-by: Abhay raj <abhay02delhi@gmail.com>

---
+++ 36-Search_Insert_Position/Search_Insert_Position_O.cpp
@@ -0,0 +1,23 @@
+// time : O(log n)
+// space : O(1)
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+    int s = 0;
+    int e = nums.size() - 1;
+    
+    while(s <= e){
+        int mid = s + (e - s) / 2;
+        
+        if(nums[mid] == target) {
+            return mid;
+        }else if(nums[mid] < target){
+            s = mid + 1;
+        }else{ // arr[mid] > target
+            e = mid - 1;
+        }
+        
+    }
+    return s;
+    }
+};
